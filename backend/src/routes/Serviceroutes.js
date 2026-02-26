c140afd0cbaa665680fe30cb2d4162c4a522d30f
jishnu-nks-2002 <jishnunarayanan23@gmail.com>
2026-02-26 14:10:34 +0530

    changes

--- backend/src/routes/Serviceroutes.js
+++ backend/src/routes/Serviceroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const serviceController = require('../controllers/serviceController');
+const serviceController = require('../controllers/Servicecontroller');
 const { protect } = require('../middleware/auth');
 const { uploadImage } = require('../middleware/uploadMiddleware');
 
